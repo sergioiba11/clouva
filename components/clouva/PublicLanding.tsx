9b72a84b9aa0f6d335b8c5d51ffc202b706b6878
Clouva <sergio.iba.11@gmail.com>
2026-08-30 12:43:10 -0300

    chore: trigger CLOUVA web deploy

--- components/clouva/PublicLanding.tsx
+++ components/clouva/PublicLanding.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { OfficialClouvaMark } from "@/components/clouva/OfficialClouvaMark";
 
+// Deploy marker: 2026-08-30 CLOUVA landing background release.
 export function PublicLanding() {
   return (
     <main className="relative min-h-[100svh] overflow-hidden bg-black text-white">
