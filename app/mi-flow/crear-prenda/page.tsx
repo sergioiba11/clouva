066ee25c2852a7688d4f9f3593430935e05f2df3
Clouva <sergio.iba.11@gmail.com>
2026-09-08 17:29:43 -0300

    feat: return garment flow results to merch creator

--- app/mi-flow/crear-prenda/page.tsx
+++ app/mi-flow/crear-prenda/page.tsx
@@ -1,11 +1,13 @@
 import GarmentFlowClient from "./GarmentFlowClient";
 import { DirectUnrealGarmentButton } from "./DirectUnrealGarmentButton";
+import { CreatorProjectBridge } from "./CreatorProjectBridge";
 
 export default function CrearPrendaPage() {
   return (
     <>
       <GarmentFlowClient />
       <DirectUnrealGarmentButton />
+      <CreatorProjectBridge />
     </>
   );
 }
