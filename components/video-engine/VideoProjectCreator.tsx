b2e0f6542737aa13182d38ef506d9b3c1d52c907
Clouva <sergio.iba.11@gmail.com>
2026-09-18 22:08:24 -0300

    video-engine: clean creator imports

--- components/video-engine/VideoProjectCreator.tsx
+++ components/video-engine/VideoProjectCreator.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @next/next/no-img-element -- generated media URLs are external GCS objects. */
 
 import Link from "next/link";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import {
   ArrowDownToLine,
   ArrowLeft,
