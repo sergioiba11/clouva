9c090ed21e25578d28c0ee82a402f5a0217f8134
Clouva <sergio.iba.11@gmail.com>
2026-09-22 09:39:41 -0300

    feat(vidadeflows): publish project route in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,6 +8,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     "/",
     "/sobre-clouva",
     "/clouva",
+    "/clouva/vidadeflows",
     "/tienda",
     "/catalogo",
     "/carrito",
